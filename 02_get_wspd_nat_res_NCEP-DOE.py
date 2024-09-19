6f6a7057596e49baef2da120d979d9a0b4b5c883
Tereza Jarnikova <tjarnik@gmail.com>
2024-09-19 14:30:18 +0100

    committing updated PL for extraction for NASA-MERRA and NCEP-DOE

--- 02_get_wspd_nat_res_NCEP-DOE.py
+++ 02_get_wspd_nat_res_NCEP-DOE.py
@@ -1,18 +1,21 @@
+#put python script here
+
 import time
 import xarray as xr
 import numpy as np
 
-#vwnd, uwnd
+for y in range(1979,2024):
+    
+    print(y)
+    tv = xr.open_dataset(f'/gpfs/data/greenocean2/software/products/windsFromComponents/NCEP-DOE/daily/vwnd.10m.gauss.{y}_daily.nc')
+    tu = xr.open_dataset(f'/gpfs/data/greenocean2/software/products/windsFromComponents/NCEP-DOE/daily/uwnd.10m.gauss.{y}_daily.nc')
 
+    q = time.time()
+    twind = xr.ufuncs.sqrt(xr.ufuncs.square(tu.uwnd) + xr.ufuncs.square(tv.vwnd))
+    twind.name = 'windspeed'
 
-td = '/gpfs/data/greenocean2/software/products/windsFromComponents/'
-ncep_doe = 'NCEP-DOE/'
+    w5 = twind.to_dataset()
+    w5.to_netcdf(f'/gpfs/data/greenocean2/software/products/windsFromComponents/NCEP-DOE/daily/wspd.10m.gauss.{y}_daily.nc')
 
-tv = xr.open_dataset(f'{td}/{ncep_doe}/data_vwnd.nc')
-tu = xr.open_dataset(f'{td}/{ncep_doe}/data_uwnd.nc')
-twind = xr.ufuncs.sqrt(xr.ufuncs.square(tu.uwnd) + xr.ufuncs.square(tv.vwnd))
-twind.name = 'windspeed'
-
-w5 = twind.to_dataset()
-w5.to_netcdf(f'{td}/{ncep_doe}/data_windspeed.nc')
-
+    q1 = time.time()
+    print(q1-q)
--- 03_regrid_NCEP-DOE.sh
+++ 03_regrid_NCEP-DOE.sh
@@ -1,16 +1,18 @@
+for i in {1979..2024}
 
+do
+    tdir='/gpfs/data/greenocean2/software/products/windsFromComponents/NCEP-DOE/daily/'
+    newfil="${tdir}vwnd.10m.gauss.${i}_daily_rg.nc"
+    oldfil="${tdir}vwnd.10m.gauss.${i}_daily.nc"
+    echo ${oldfil}
+    cdo remapbil,r360x180 ${oldfil} ${newfil}
 
-#tdir='/gpfs/data/greenocean2/software/products/windsFromComponents/NCEP-DOE/'
-#newfil="${tdir}data_vwnd_rg.nc"
-#oldfil="${tdir}data_vwnd.nc"
-#cdo remapbil,r360x180 ${oldfil} ${newfil}
+    newfil="${tdir}uwnd.10m.gauss.${i}_daily_rg.nc"
+    oldfil="${tdir}uwnd.10m.gauss.${i}_daily.nc"
+    cdo remapbil,r360x180 ${oldfil} ${newfil}
 
-tdir='/gpfs/data/greenocean2/software/products/windsFromComponents/NCEP-DOE/'
-newfil="${tdir}data_uwnd_rg.nc"
-oldfil="${tdir}data_uwnd.nc"
-cdo remapbil,r360x180 ${oldfil} ${newfil}
+    newfil="${tdir}wspd.10m.gauss.${i}_daily_rg.nc"
+    oldfil="${tdir}wspd.10m.gauss.${i}_daily.nc"
+    cdo remapbil,r360x180 ${oldfil} ${newfil}
 
-tdir='/gpfs/data/greenocean2/software/products/windsFromComponents/NCEP-DOE/'
-newfil="${tdir}data_windspeed_rg.nc"
-oldfil="${tdir}data_windspeed.nc"
-cdo remapbil,r360x180 ${oldfil} ${newfil}
+done
